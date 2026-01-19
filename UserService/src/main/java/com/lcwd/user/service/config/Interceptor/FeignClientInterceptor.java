3b12c5d008837f3ece560e8c5124926bce9009fd
shubhanshu.patel <shubhanshupatel2017@gmail.com>
2026-01-19 23:28:09 +0530

    Interceptor Changes Added

--- UserService/src/main/java/com/lcwd/user/service/config/Interceptor/FeignClientInterceptor.java
+++ UserService/src/main/java/com/lcwd/user/service/config/Interceptor/FeignClientInterceptor.java
@@ -1,4 +1,23 @@
 package com.lcwd.user.service.config.Interceptor;
 
-public class FeignClientInterceptor {
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@Component
+public class FeignClientInterceptor implements RequestInterceptor {
+
+    private OAuth2AuthorizedClientManager manager;
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        String token = manager.authorize(
+                OAuth2AuthorizeRequest.withClientRegistrationId("my-internal-client").build())
+                .getAccessToken().getTokenValue();
+        requestTemplate.header("Authorization", "Bearer " + token);
+    }
 }
